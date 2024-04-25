024e9c28b51afc99c8cfe6e57817ee197f640204
Dwij13 <dwijjindal2005@gmail.com>
2024-04-25 16:08:09 +0530

    Complete

---
+++ react-tailwind/src/App.jsx
@@ -0,0 +1,20 @@
+
+import React from "react";
+import Navbar from "./Components/Navbar";
+import Alert from "./Components/Alert";
+import Card from "./Components/Card";
+import Footer from "./Components/Footer";
+
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Alert />
+      <Card />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
